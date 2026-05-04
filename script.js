be50215287557e73ac20f07e03df8f6709823e0e
MD ASIF <145357545+MD-ASIF-coder@users.noreply.github.com>
2026-05-04 12:00:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+
+const inputs = document.querySelectorAll('.code');
+
+inputs.forEach((input, i) => {
+	input.addEventListener('keyup', (e)=>{
+		if (e.key >= '0' && e.key <= '9') {
+			inputs[i + 1].focus();
+		}
+
+		else if (e.key == 'Backspace') {
+			input.value = '';
+			inputs[i - 1].focus();
+		}
+
+		else {
+			e.preventDefault();
+		}
+	})
+})
